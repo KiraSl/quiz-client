90221ee811df61fcb58ed0fec300339a65c8ed7a
Sam De Maeyer <sam_demaeyer@hotmail.com>
2020-03-30 13:23:51 +0100

    Add react-syntax-highlighter and add example to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import './App.css'
 import { Button } from 'react-bootstrap'
+import SyntaxHighlighter from 'react-syntax-highlighter'
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 
 function App() {
+  const codeString = `
+    function foo() {
+      // do stuff
+    }
+  `
   return (
     <div className="App">
       <Button>Quiz client</Button>
+      <SyntaxHighlighter language="javascript" style={docco}>
+        {codeString}
+      </SyntaxHighlighter>
     </div>
   )
 }
