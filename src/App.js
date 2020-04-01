54e1c1ea2491eb9a015e7ebd4522b368226faacf
Kira Slobodina <slobodina.kira@gmail.com>
2020-04-01 18:42:25 +0100

    First bootstrap classes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './App.css'
-import { Button } from 'react-bootstrap'
 
 function App() {
   return (
-    <div className="App">
-      <Button>Quiz client</Button>
+    <div className="d-flex align-items-center justify-content-center vh-100 flex-column">
+      <h1>Javascript</h1>
+      <div>1/10</div>
     </div>
   )
 }
