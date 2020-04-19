4093d5305b59c872f685182f78349e4a61f56392
Kira Slobodina <slobodina.kira@gmail.com>
2020-04-19 23:37:28 +0100

    Add syntax highlighter and make current question number dynamic

--- src/routes/Game.js
+++ src/routes/Game.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import SyntaxHighlighter from 'react-syntax-highlighter'
+import { dracula } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 
 class Game extends React.Component {
   constructor(props) {
@@ -7,6 +9,7 @@ class Game extends React.Component {
 
     this.state = {
       questions: [],
+      currentQuestionIndex: 0,
     }
   }
 
@@ -19,10 +22,25 @@ class Game extends React.Component {
   render() {
     const { categoryId } = this.props.match.params
     const category = this.props.categories.find(category => category.id.toString() === categoryId)
+    const question = this.state.questions[this.state.currentQuestionIndex]
+
+    // Next: create a component to render the question & create a "Next" button to move to the next question. 
     return (
       <div className="d-flex align-items-center justify-content-center vh-100 flex-column">
         {category && <h1>{category.name}</h1>}
-        <div>1/{this.state.questions.length}</div>
+        {question ?
+          <>
+            <div>{this.state.currentQuestionIndex + 1} /{this.state.questions.length}</div>
+            <h3>{question.title}</h3>
+            <p>{question.description}</p>
+            {question.codeBlock &&
+              <SyntaxHighlighter language={question.codeType} style={dracula}>
+                {question.codeBlock}
+              </SyntaxHighlighter>
+            }
+          </> :
+          <h3>Loading...</h3>
+        }
       </div >
     )
   }
